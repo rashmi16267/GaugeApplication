b4e51a706e519635451b17c10d294b24d0443e63
Rashmi Rao <67630400+rashmi16267@users.noreply.github.com>
2021-08-10 19:32:30 +0530

    Add files via upload

---
+++ AndroidStudioFiles/MyApplication/app/src/main/java/com/example/myapplication/ContactUsPage.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class ContactUsPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactuspage);
+    }
+
+    public void goToMenuFromContactUs (View view){
+        Intent intent = new Intent (this, MenuPage.class);
+        startActivity(intent);
+    }
+    public void SubmitContactUsInfo (View view){
+        //code
+    }
+
+}
